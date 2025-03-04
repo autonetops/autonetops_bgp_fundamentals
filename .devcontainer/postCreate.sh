1162c095c05f73e428318519b39bd2574504d05d
pdamasceno <pedro.damasceno@hotmail.com>
2025-03-04 16:14:38 +0100

    update

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -6,4 +6,6 @@ set +e
 # CODESPACE_NAME is created by the codespaces by default
 echo 'export TF_VAR_VPN_CODESPACES_2222="https://$CODESPACE_NAME-2222.app.github.dev"' >> ~/.bashrc
 echo 'export TF_VAR_VPN_CODESPACES_IP=$(curl -s ifconfig.me)' >> ~/.bashrc
-source ~/.bashrc
+
+echo 'export TF_VAR_VPN_CODESPACES_2222="https://$CODESPACE_NAME-2222.app.github.dev"'
+echo 'export TF_VAR_VPN_CODESPACES_IP=$(curl -s ifconfig.me)'
