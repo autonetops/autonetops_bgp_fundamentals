2b557b42a4e2c5a9384da5edfe9ed522d3c790da
pdamasceno <pedro.damasceno@hotmail.com>
2025-03-04 15:29:26 +0100

    update

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -6,3 +6,6 @@ set +e
 # CODESPACE_NAME is created by the codespaces by default
 echo 'export TF_VAR_VPN_CODESPACES_2222="https://$CODESPACE_NAME-2222.app.github.dev"' >> ~/.bashrc
 echo 'export TF_VAR_VPN_CODESPACES_IP=$(curl -s ifconfig.me)' >> ~/.bashrc
+source ~/.bashrc
+
+docker pull ghcr.io/autonetops/autonetops_labs/cisco_iol:latest
